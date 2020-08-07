582a0d0e8b148ebcccbf79d5fad51a057b8bf496
小二 <1248774172@qq.com>
2020-08-07 13:58:20 +0800

    加入了布局

---
+++ CircleButton/build/generated/source/buildConfig/debug/at/markushi/circlebutton/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package at.markushi.circlebutton;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String LIBRARY_PACKAGE_NAME = "at.markushi.circlebutton";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
